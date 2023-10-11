ad5ef35a77a9dad29dd7d0d24f652eec1300f786
Rohan Grover <rohan7grover@gmail.com>
2023-10-11 15:41:35 +0530

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Home from "./pages/Home";
-// import Product from "./pages/Product";
-// import ProductList from "./pages/ProductList";
-// import Register from "./pages/Register";
-// import Login from "./pages/Login";
-// import Cart from "./pages/Cart";
+
+import {
+  BrowserRouter, Route, Routes
+} from "react-router-dom";
 
 const App = () => {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
