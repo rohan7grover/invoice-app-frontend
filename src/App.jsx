07132d4279032e0b9c9a4fc7e4ddf00df6501dea
Rohan Grover <rohan7grover@gmail.com>
2023-10-11 13:31:42 +0530

    completed frontend design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-// import Product from "./pages/Product";
 import Home from "./pages/Home";
+// import Product from "./pages/Product";
 // import ProductList from "./pages/ProductList";
 // import Register from "./pages/Register";
 // import Login from "./pages/Login";
